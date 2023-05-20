8ba86500adeb7e58a97e63cb84f0845969f70a04
Virginie <vb@ikimo9.com>
2023-05-20 08:33:26 +0200

    ajout des blades blog.index et blog.show

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,23 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Contracts\View\View;
+
 
 class BlogController extends Controller
 {
-    public function index(): LengthAwarePaginator
+    public function index(): View
     {
-        return Post::paginate(25);
+        return view('blog.index', [
+            'posts' => Post::paginate(1)
+        ]);
     }
 
-    public function show(string $slug, string $id): RedirectResponse | Post
+    public function show(string $slug, string $id): View
     {
         $post = Post::findorFail($id);
         if ($post->slug !== $slug) {
             return to_route('blog.show', ['slug' => $post->slug, 'id' => $post->id]);
         }
-        return $post;
+        return view('blog.show', [
+            'post' => $post
+        ]);
     }
 }
